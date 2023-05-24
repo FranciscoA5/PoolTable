0d9b0071c7073b61d8847605a514f9235c005c46
TostaMista30 <f-infsec03@hotmail.com>
2023-05-24 02:22:29 +0100

    Cube draw

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,7 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 #include <iostream>
-
+#include <GL/glu.h>
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "opengl32.lib")
@@ -11,6 +11,50 @@
 extern "C" {
 	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 }
+
+void drawCube()
+{
+    GLfloat vertices[] =
+    {
+        -1, -1, -1,   -1, -1,  1,   -1,  1,  1,   -1,  1, -1,
+        1, -1, -1,    1, -1,  1,    1,  1,  1,    1,  1, -1,
+        -1, -1, -1,   -1, -1,  1,    1, -1,  1,    1, -1, -1,
+        -1,  1, -1,   -1,  1,  1,    1,  1,  1,    1,  1, -1,
+        -1, -1, -1,   -1,  1, -1,    1,  1, -1,    1, -1, -1,
+        -1, -1,  1,   -1,  1,  1,    1,  1,  1,    1, -1,  1
+    };
+
+    GLfloat colors[] =
+    {
+        0, 0, 0,   0, 0, 1,   0, 1, 1,   0, 1, 0,
+        1, 0, 0,   1, 0, 1,   1, 1, 1,   1, 1, 0,
+        0, 0, 0,   0, 0, 1,   1, 0, 1,   1, 0, 0,
+        0, 1, 0,   0, 1, 1,   1, 1, 1,   1, 1, 0,
+        0, 0, 0,   0, 1, 0,   1, 1, 0,   1, 0, 0,
+        0, 0, 1,   0, 1, 1,   1, 1, 1,   1, 0, 1
+    };
+
+    static float alpha = 0;
+    //attempt to rotate cube
+    glRotatef(50, 1, 1, 0);
+
+    /* We have a color array and a vertex array */
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glColorPointer(3, GL_FLOAT, 0, colors);
+
+    /* Send data : 24 vertices */
+    glDrawArrays(GL_QUADS, 0, 24);
+
+    /* Cleanup states */
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    alpha += 1;
+}
+
+
+
 
 int main(void) {
 	GLFWwindow* window;
@@ -27,7 +71,18 @@ int main(void) {
 	glfwMakeContextCurrent(window);//Tornar o contexto da janela na thread atual
 
 	while (!glfwWindowShouldClose(window)) {//Espera pelo pedido de fecho da janela(Alt-F4 ou botão)
-		glClear(GL_COLOR_BUFFER_BIT);
+        
+        glClearColor(0.0, 0.8, 0.3, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION_MATRIX);
+        glLoadIdentity();
+        gluPerspective(60, 800.0/ 600.0, 0.1, 100);
+
+        glMatrixMode(GL_MODELVIEW_MATRIX);
+        glTranslatef(0, 0, -5);
+
+        drawCube();
 		glfwSwapBuffers(window);//realizada apos a execucao de todos os commandos de renderizaçao de frames
 
 		//realizada apenas na thread principal, processa eventos que ja tenham sido recebidos, feito apos swapbuffers e é mais para jogos
